99a9c82f60546338f5aff4303befb63728730234
Homero Lozano <homeronicolaslozano@gmail.com>
2024-03-27 12:18:47 -0400

    Using the Array Find Method

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+function superbowlWin(record) {
+    const winningEntry = record.find(entry => entry.result === "W");
+    return winningEntry ? winningEntry.year : undefined;
+  }
+  
+  module.exports = superbowlWin;
+
+  
+
+
+
+
 // code your solution here
